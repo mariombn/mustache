64aec98a24bc7301b4f97914faa15c0da39ae52b
Mario de Moraes Barros Neto <mariombn@gmail.com>
2016-05-10 16:54:15 -0300

    Alteração para Produção

--- config.php
+++ config.php
@@ -1,18 +1,18 @@
 <?php
-//Configurações do Framework
+//ConfiguraÃ§Ãµes do Framework
 define( 'APPPATH', dirname( __FILE__ ) );
 define( 'DB_HOSTNAME', '127.0.0.1' );
 define( 'DB_USERNAME', 'root' );
 define( 'DB_PASSWORD', '' );
 define( 'DB_DATABASE', '' );
 define( 'DB_CHARSET', 'utf-8' );
-define( 'DEBUG', true ); // Troque o valor para FALSE em um ambiente de Produção
+define( 'DEBUG', true ); // Troque o valor para FALSE em um ambiente de ProduÃ§Ã£o
 
 define( 'APP_NAME', 'PROJECT-NAME' );
 define( 'APP_COPYRIGHT', '&copy; 2016 - All rights reserved' );
 define( 'APP_OWNER', 'AUTHOR-NAME' );
 define( 'APP_DESCRIPTION', 'DESCRIPTION-PROJECT' );
-define( 'HOME_PATH', 'http://mustache.dev/' ); // Para evitar problemas, sempre use um Virtual Host no seu Servidor WEB para rodar esse Framework
+define( 'HOME_PATH', 'http://mustache.mariombn.com/' ); // Para evitar problemas, sempre use um Virtual Host no seu Servidor WEB para rodar esse Framework
 
 define( 'APP_VERSION_BIG', '0' );
 define( 'APP_VERSION_MID', '0' );
